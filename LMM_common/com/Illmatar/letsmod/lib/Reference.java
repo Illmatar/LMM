d873e2554974e409b82ee52dbf28df3c84b1b93d
Illmatar <killmatar@gmail.com>
2013-04-18 23:56:52 -0400

    Ep5

---
+++ LMM_common/com/Illmatar/letsmod/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.Illmatar.letsmod.lib;
+
+public class Reference
+{
+    public static final String  MOD_ID = "LMM";
+    public static final String MOD_NAME = "Let's Mod Mod";
+    public static final String VERSION = "0.0.1";
+}
